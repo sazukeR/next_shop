64d0bdb7f125449b2a4d6da27d7efd00fe50a117
SazukeR <reinaldojcg1@gmail.com>
2023-12-27 09:07:52 -0500

    Add styles to login-page and new-account-page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,6 +2,7 @@ import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
  initialData.products[0],
@@ -10,6 +11,8 @@ const productsInCart = [
 ];
 
 export default function () {
+ // redirect("/empty");
+
  return (
   <div className='flex justify-center items-center mb-72 px-10 sm:px-0'>
    <div className='flex flex-col w-[1000px]'>

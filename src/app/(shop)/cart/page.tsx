c8b2c2cae97a2e7c05d72a86f83f68d764a385b1
SazukeR <reinaldojcg1@gmail.com>
2023-12-27 09:13:53 -0500

    Fix prices box on cart-page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -56,7 +56,7 @@ export default function () {
 
      {/* checkout - resumen de orden */}
 
-     <div className='bg-white rounded-xl shadow-xl p-7'>
+     <div className=' bg-white rounded-xl shadow-xl p-7 h-fit'>
       <h2 className='text-2xl'>Resumen de orden</h2>
       <div className='grid grid-cols-2'>
        <span>No. Productos</span>

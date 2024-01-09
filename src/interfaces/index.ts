8f1a389a47ec24ea2db034f8c640115156d5536a
SazukeR <reinaldojcg1@gmail.com>
2024-01-09 17:24:27 -0500

    Add config to address ui, database and actions

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,3 @@
 export * from "./products.interface"
+export * from "./country.interface"
+export * from "./address.interface"
--- src/seed/seedDatabase.ts
+++ src/seed/seedDatabase.ts
@@ -1,12 +1,16 @@
 import prisma from '../lib/prisma';
 import { initialData } from "./seed";
+import { countries } from './seed-countries';
 
 
 
 async function main() {
 // borrar registros previos
   /*   await Promise.all([ */
+    await prisma.userAddress.deleteMany();
     await prisma.user.deleteMany();
+    await prisma.country.deleteMany();
+
     await prisma.productImage.deleteMany();
     await prisma.product.deleteMany();
     await prisma.category.deleteMany();
@@ -16,6 +20,10 @@ async function main() {
 
     await prisma.user.createMany({
         data: users
+    });
+
+    await prisma.country.createMany({
+        data: countries
     });
 
 // categorias
